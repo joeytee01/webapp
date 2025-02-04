176f343879b3993b52ded3857cd5f9f18df26b99
joey.teewh@gmail.com <joey.teewh@gmail.com>
2025-02-04 13:55:50 +0800

    Update app listen

--- app.js
+++ app.js
@@ -99,6 +99,6 @@ app.get('/delete-item/:id', (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on http://0.0.0.0:3000');
 });
