61938df8ac82f87cef24582479de9fd01147fcd4
joey.teewh@gmail.com <joey.teewh@gmail.com>
2025-02-04 22:45:39 +0800

    Update port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('./db'); // Import the database connection
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
